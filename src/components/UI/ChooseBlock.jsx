3f9a407684e263c1c4fd7aa6563155ea88cead67
bombasters <59049797+bombasters@users.noreply.github.com>
2023-04-29 20:51:05 +0300

    Add files via upload

--- src/components/UI/ChooseBlock.jsx
+++ src/components/UI/ChooseBlock.jsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 const ChooseBlock = ({logo, label, content}) => {
   return (
     <div className={'p-[32px] border flex flex-col gap-[16px] hover:bg-[#FFD050] transition-all delay-150 cursor-pointer'}>
-        <Image src={logo}/>
+        <div className={'bg-[#FBF6EA] p-[12px] rounded-[5px] w-max'}>
+            <Image src={logo}/>
+        </div>
         <div className={'flex flex-col gap-[4px]'}>
             <h1 className={'text-[#232536] text-[28px] font-bold'}>{label}</h1>
             <span className={'text-[#6D6E76] text-[16px]'}>{content}</span>
